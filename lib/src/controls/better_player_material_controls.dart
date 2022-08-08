681d195585b30d590e55a3183553a55b4a6a344d
Nazish bahadar <nazishpathan7861234@gmail.com>
2022-08-08 17:24:57 +0530

    provided adress for set fit to player change fit

--- lib/src/controls/better_player_material_controls.dart
+++ lib/src/controls/better_player_material_controls.dart
@@ -1000,7 +1000,7 @@ class _BetterPlayerMaterialControlsState
               Spacer(flex: 5,),
               PlayerChangeFit(
                 controller: _betterPlayerController,
-                setFit: CustomBetterPlayer.betterPlayer.setFit
+                setFit: CustomBetterPlayer.customBetterPlayerState.betterPlayer.setFit
               ),
               // _buildMoreButton(),
             ],
